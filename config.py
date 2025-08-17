50dea3e3772762edfdbb2389dcc94f5f09138324
Lai <jasonlai3481@gmail.com>
2025-08-17 11:00:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.cjcdnhx7mshq.us-east-1.rds.amazonaws.com"
 customuser = "awsuser"
 custompass = "Bmit3273"
 customdb = "employee"
-custombucket = "laiyunglim-employee"
+custombucket = "laiyunglim-bucket"
 customregion = "us-east-1"
 
