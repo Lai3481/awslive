5f74570995849b9f8bc55c28b7de700139a54b41
Lai <jasonlai3481@gmail.com>
2025-08-17 10:43:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cjcdnhx7mshq.us-east-1.rds.amazonaws.com"
+customuser = "awsuser"
+custompass = "Bmit3273"
+customdb = "employee"
+custombucket = "laiyunglim-employee"
+customregion = "us-east-1"
+
